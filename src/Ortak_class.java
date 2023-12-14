9ee6beef81679be27cd77b46850c2ffc2ffd4c9c
Senaydaakkaya <senaydaakkaya@gmail.com>
2023-12-14 11:40:50 +0300

    first commit

---
+++ src/Ortak_class.java
@@ -0,0 +1,7 @@
+public class Ortak_class {
+
+    public static void main(String[] args) {
+
+        System.out.println("Buradan bir Senaydaakkaya geçti");
+    }
+}
---
+++ src/senaydaakkaya.java
@@ -0,0 +1,15 @@
+public class senaydaakkaya {
+    public static void main(String[] args) {
+
+
+        System.out.println("Senaydaakkaya'nın Yeri");
+        /*
+        git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Senaydaakkaya/GitHubTeamWork_T127.git
+git push -u origin main
+         */
+    }
+}
